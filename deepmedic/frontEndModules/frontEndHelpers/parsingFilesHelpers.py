dc7d5dd92a7b60df41e6546290c66b8938084e6a
Konstantinos Kamnitsas <konstantinos.kamnitsas12@imperial.ac.uk>
2016-07-07 15:27:08 +0100

    corrected path parsing to support windows too

--- deepmedic/frontEndModules/frontEndHelpers/parsingFilesHelpers.py
+++ deepmedic/frontEndModules/frontEndHelpers/parsingFilesHelpers.py
@@ -6,30 +6,26 @@
 # or read the terms at https://opensource.org/licenses/BSD-3-Clause.
 
 import os
-#generic
-def getAbsPathEvenIfRelativeIsGiven(pathGiven, absolutePathToWhereRelativePathRelatesTo) :
-	#os.path.abspath "cleans" Additional ../.// etc.
 
-	if pathGiven.startswith("/") : 
-		return os.path.abspath(pathGiven)
+def getAbsPathEvenIfRelativeIsGiven(pathGiven, absolutePathToWhereRelativePathRelatesTo) :
+	#os.path.normpath "cleans" Additional ../.// etc.
+	if os.path.isabs(pathGiven) : 
+		return os.path.normpath(pathGiven)
 	else : #relative path given. Need to make absolute path
 		if os.path.isdir(absolutePathToWhereRelativePathRelatesTo) :
 			relativePathToWhatGiven = absolutePathToWhereRelativePathRelatesTo
 		elif os.path.isfile(absolutePathToWhereRelativePathRelatesTo) :
-			relativePathToWhatGiven = absolutePathToWhereRelativePathRelatesTo[: absolutePathToWhereRelativePathRelatesTo.rfind("/")] + "/"
+			relativePathToWhatGiven = os.path.dirname(absolutePathToWhereRelativePathRelatesTo)
 		else : #not file, not dir, exit.
 			print "ERROR: in [func:returnAbsolutePathEvenIfRelativePathIsGiven()] Given path :", absolutePathToWhereRelativePathRelatesTo, " does not correspond to neither an existing file nor a directory. Exiting!"; exit(1)
-		return os.path.abspath(relativePathToWhatGiven + "/" + pathGiven)
+		return os.path.normpath(relativePathToWhatGiven + "/" + pathGiven)
 
-#Generic.
 def checkIfAllElementsOfAListAreFilesAndExitIfNot(pathToTheListingFile, list1) :
 	for filepath in list1 :
 		if not os.path.isfile(filepath) :
 			print "ERROR: in [checkIfAllElementsOfAListExistAndExitIfNot()] path:", filepath, " given in :", pathToTheListingFile," does not correspond to a file. Exiting!"
 			print exit(1)
 
-
-#Generic.
 def parseFileLinesInList(pathToListingFile) :
 	list1 = []
 	with open(pathToListingFile, "r") as inp :
@@ -39,26 +35,24 @@ def parseFileLinesInList(pathToListingFile) :
 	return list1
 
 def parseAbsFileLinesInList(pathToListingFile) :
-	# os.path.abspath below is to "clean" the paths from ./..//...
-	pathToFolderContainingThisListFile = pathToListingFile[: pathToListingFile.rfind("/")] + "/"
+	# os.path.normpath below is to "clean" the paths from ./..//...
+	pathToFolderContainingThisListFile = os.path.dirname(pathToListingFile)
 	list1 = []
 	with open(pathToListingFile, "r") as inp :
 		for line in inp :
 			if not line.startswith("#") and line.strip() <> "" :
 				pathToFileParsed = line.strip()
-				if pathToFileParsed.startswith("/") : #abs path.
-					list1.append(os.path.abspath(pathToFileParsed))
+				if os.path.isabs(pathToFileParsed) : #abs path.
+					list1.append(os.path.normpath(pathToFileParsed))
 				else : #relative path to this listing-file.
-					list1.append(os.path.abspath(pathToFolderContainingThisListFile + "/" + pathToFileParsed))
+					list1.append(os.path.normpath(pathToFolderContainingThisListFile + "/" + pathToFileParsed))
 	return list1
 
-#Generic.
 def checkListContainsCorrectNumberOfCasesOtherwiseExitWithError(numberOfCasesPreviously, pathToGivenListFile, listOfFilepathsToChannelIForEachCase) :
 	numberOfContainedCasesInList = len(listOfFilepathsToChannelIForEachCase)
 	if numberOfCasesPreviously <> numberOfContainedCasesInList :
 		print "ERROR: Given file:", pathToGivenListFile, " contains #", numberOfContainedCasesInList," entries, whereas previously checked files contained #", numberOfCasesPreviously,". All listing-files for channels, masks, etc, should contain the same number of entries, one for each case.\nExiting!"
 		exit(1)
-
 
 def checkThatAllEntriesOfAListFollowNameConventions(listOfPredictionNamesForEachCaseInListingFile) :
 	for entry in listOfPredictionNamesForEachCaseInListingFile :
