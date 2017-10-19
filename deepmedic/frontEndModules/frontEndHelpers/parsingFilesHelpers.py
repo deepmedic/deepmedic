4ca63cca89a7319ba1af6bdf61a18f6982c167f1
Joeri Nicolaes <joerinicolaes@gmail.com>
2017-10-19 14:54:14 +0200

    refactoring for py3 by changing print to function call print() and replacing <> by !=

--- deepmedic/frontEndModules/frontEndHelpers/parsingFilesHelpers.py
+++ deepmedic/frontEndModules/frontEndHelpers/parsingFilesHelpers.py
@@ -17,20 +17,20 @@ def getAbsPathEvenIfRelativeIsGiven(pathGiven, absolutePathToWhereRelativePathRe
         elif os.path.isfile(absolutePathToWhereRelativePathRelatesTo) :
             relativePathToWhatGiven = os.path.dirname(absolutePathToWhereRelativePathRelatesTo)
         else : #not file, not dir, exit.
-            print "ERROR: in [func:returnAbsolutePathEvenIfRelativePathIsGiven()] Given path :", absolutePathToWhereRelativePathRelatesTo, " does not correspond to neither an existing file nor a directory. Exiting!"; exit(1)
+            print("ERROR: in [func:returnAbsolutePathEvenIfRelativePathIsGiven()] Given path :", absolutePathToWhereRelativePathRelatesTo, " does not correspond to neither an existing file nor a directory. Exiting!"); exit(1)
         return os.path.normpath(relativePathToWhatGiven + "/" + pathGiven)
     
 def checkIfAllElementsOfAListAreFilesAndExitIfNot(pathToTheListingFile, list1) :
     for filepath in list1 :
         if not os.path.isfile(filepath) :
-            print "ERROR: in [checkIfAllElementsOfAListExistAndExitIfNot()] path:", filepath, " given in :", pathToTheListingFile," does not correspond to a file. Exiting!"
-            print exit(1)
+            print("ERROR: in [checkIfAllElementsOfAListExistAndExitIfNot()] path:", filepath, " given in :", pathToTheListingFile," does not correspond to a file. Exiting!")
+            print(exit(1))
             
 def parseFileLinesInList(pathToListingFile) :
     list1 = []
     with open(pathToListingFile, "r") as inp :
         for line in inp :
-            if not line.startswith("#") and line.strip() <> "" :
+            if not line.startswith("#") and line.strip() != "" :
                 list1.append(line.strip())
     return list1
 
@@ -42,7 +42,7 @@ def parseAbsFileLinesInList(pathToListingFile) :
         for line in inp :
             if line.strip() == "-" : # symbol indicating the non existence of this channel. Will be zero-filled.
                 list1.append("-")
-            elif not line.startswith("#") and line.strip() <> "" :
+            elif not line.startswith("#") and line.strip() != "" :
                 pathToFileParsed = line.strip()
                 if os.path.isabs(pathToFileParsed) : #abs path.
                     list1.append(os.path.normpath(pathToFileParsed))
@@ -52,12 +52,12 @@ def parseAbsFileLinesInList(pathToListingFile) :
 
 def checkListContainsCorrectNumberOfCasesOtherwiseExitWithError(numberOfCasesPreviously, pathToGivenListFile, listOfFilepathsToChannelIForEachCase) :
     numberOfContainedCasesInList = len(listOfFilepathsToChannelIForEachCase)
-    if numberOfCasesPreviously <> numberOfContainedCasesInList :
-        print "ERROR: Given file:", pathToGivenListFile, " contains #", numberOfContainedCasesInList," entries, whereas previously checked files contained #", numberOfCasesPreviously,". All listing-files for channels, masks, etc, should contain the same number of entries, one for each case.\nExiting!"
+    if numberOfCasesPreviously != numberOfContainedCasesInList :
+        print("ERROR: Given file:", pathToGivenListFile, " contains #", numberOfContainedCasesInList," entries, whereas previously checked files contained #", numberOfCasesPreviously,". All listing-files for channels, masks, etc, should contain the same number of entries, one for each case.\nExiting!")
         exit(1)
         
 def checkThatAllEntriesOfAListFollowNameConventions(listOfPredictionNamesForEachCaseInListingFile) :
     for entry in listOfPredictionNamesForEachCaseInListingFile :
         if entry.find("/") > -1 or entry.startswith(".") :
-            print "ERROR: in [checkThatAllEntriesOfAListFollowNameConventions()] while checking that all entries follow name-conventions. Entry \"", entry, "\" was found to begin with \'.\' or contain \'/\'. Please correct. Exiting!"
-            print exit(1)
+            print("ERROR: in [checkThatAllEntriesOfAListFollowNameConventions()] while checking that all entries follow name-conventions. Entry \"", entry, "\" was found to begin with \'.\' or contain \'/\'. Please correct. Exiting!")
+            print(exit(1))
