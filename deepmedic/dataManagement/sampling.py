bec56ed13b758d4bcca22a1fb89fdbfd6e86777f
Konstantinos Kamnitsas <konstantinos.kamnitsas12@imperial.ac.uk>
2020-01-29 08:55:45 +0000

    Corrected timing in sampling.py.

--- deepmedic/dataManagement/sampling.py
+++ deepmedic/dataManagement/sampling.py
@@ -328,7 +328,9 @@ def load_subj_and_sample(job_idx,
     # Get number of samples per sampling-category for the specific subject (class, foregr/backgr, etc)
     (n_samples_per_cat, valid_cats) = sampling_type.distribute_n_samples_to_categs(n_samples_per_subj[job_idx],
                                                                                    sampling_maps_per_cat)
-
+    time_sample_idxs = 0
+    time_extr_samples = 0
+    time_augm_samples = 0
     str_samples_per_cat = " Done. Samples per category: "
     for cat_i in range(sampling_type.get_n_sampling_cats()):
         cat_str = sampling_type.get_sampling_cats_as_str()[cat_i]
@@ -342,7 +344,7 @@ def load_subj_and_sample(job_idx,
             assert n_samples_for_cat == 0
             continue # This should not be needed, the next func should also handle it. But whatever.
             
-        time_extr_sample_0 = time.time()
+        time_sample_idx0 = time.time()
         (idxs_sampl_centers,
          slice_idxs_sampl_segms) = sample_idxs_of_segments(log,
                                                            job_id,
@@ -350,11 +352,10 @@ def load_subj_and_sample(job_idx,
                                                            dims_hres_segment,
                                                            dims_of_scan,
                                                            sampling_map)
+        time_sample_idxs += time.time() - time_sample_idx0
         str_samples_per_cat += "[" + cat_str + ": " + str(len(idxs_sampl_centers[0])) + "/" + str(n_samples_for_cat) + "] "
-
+        
         # Use the just sampled coordinates of slices to actually extract the segments (data) from the subject's images.
-        time_extr_samples = time.time() - time_extr_sample_0 # Count sampling of idx together with actual extract.
-        time_augm_samples = 0
         for image_part_i in range(len(idxs_sampl_centers[0])):
             coord_center = idxs_sampl_centers[:, image_part_i]
             
@@ -386,6 +387,7 @@ def load_subj_and_sample(job_idx,
                "[Load: {0:.1f}".format(time_load) + "] "
                "[Preproc: {0:.1f}".format(time_prep) + "] " +
                "[Augm-Img: {0:.1f}".format(time_augm_img) + "] " +
+               "[Sample Coords: {0:.1f}".format(time_sample_idxs) + "] " +
                "[Extract Sampl: {0:.1f}".format(time_extr_samples) + "] " +
                "[Augm-Samples: {0:.1f}".format(time_augm_samples) + "] secs")
     return (channs_of_samples_per_path, lbls_predicted_part_of_samples)
