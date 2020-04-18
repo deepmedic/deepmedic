4b2bb54f90501f87432a66e78cace26d323d7ec4
kk2412 <konstantinos.kamnitsas12@imperial.ac.uk>
2020-04-18 15:29:19 +0100

    Fixed problem with tensorboard events not appearing in TF2.

--- deepmedic/logging/tensorboard_logger.py
+++ deepmedic/logging/tensorboard_logger.py
@@ -6,11 +6,10 @@ import tensorflow as tf
 class TensorboardLogger(object):
 
     def __init__(self, log_path, tf_graph):
-        #_ = tf.compat.v1.summary.FileWriter(log_path, tf_graph)
-        self.logger = tf.summary.create_file_writer(log_path) # tf.compat.v1.summary.FileWriter(log_path, tf_graph)
+        self.logger = tf.compat.v1.summary.FileWriter(log_path, tf_graph)
 
     def add_summary(self, value, name, step_num):
-        #self.logger.add_summary(tf.compat.v1.Summary(value=[tf.compat.v1.Summary.Value(tag=name, simple_value=value)]),
-        #                        global_step=step_num)
-        with self.logger.as_default():
-            tf.summary.scalar(name=name, data=value, step=step_num)
+        self.logger.add_summary(tf.compat.v1.Summary(value=[tf.compat.v1.Summary.Value(tag=name, simple_value=value)]),
+                                global_step=step_num)
+        self.logger.flush()
+
